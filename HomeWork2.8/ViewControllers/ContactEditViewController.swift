63c8324f695fbc1c598a447dfe7b0ebf065d1bab
Vyacheslav Efimenko <sakhguitarkitty@icloud.com>
2021-02-21 03:30:55 +1100

    -changer person model property from let to var so we can edit model -add main logic for editVC

--- HomeWork2.8/ViewControllers/ContactEditViewController.swift
+++ HomeWork2.8/ViewControllers/ContactEditViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class ContactEditViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var contactNameTextField: UITextField!
+    @IBOutlet var contactSurnameTextFiled: UITextField!
+    @IBOutlet var contactPhoneTextFiled: UITextField!
+    @IBOutlet var contactEmailTextFiled: UITextField!
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        contactNameTextField.text = person.name
+        contactSurnameTextFiled.text = person.surname
+        contactPhoneTextFiled.text = person.phoneNumber
+        contactEmailTextFiled.text = person.email
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    @IBAction func saveButtonPressed() {
+        if contactNameTextField.text != nil || contactPhoneTextFiled.text != nil {
+            showAllert(title: "Error!", message: "Phone or name not entered, try again!")
+        }
+        saveEditedData()
     }
-    */
-
+    
+    // MARK: - Alert Controller
+    private func showAllert(title: String?, message: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertOkButton = UIAlertAction(title: "OK",style: .default)
+        alert.addAction(alertOkButton)
+        present(alert, animated: true)
+    }
+    
+    // MARK: - Work with keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == contactNameTextField{
+            contactSurnameTextFiled.becomeFirstResponder()
+        } else if textField == contactSurnameTextFiled{
+            contactPhoneTextFiled.becomeFirstResponder()
+        } else if textField == contactEmailTextFiled{
+            contactEmailTextFiled.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+        }
+    }
+    
+//MARK: - Extensions
+extension ContactEditViewController {
+    func saveEditedData() {
+        person.name = contactNameTextField.text ?? ""
+        person.surname = contactSurnameTextFiled.text ?? ""
+        person.phoneNumber = contactPhoneTextFiled.text ?? ""
+        person.email = contactEmailTextFiled.text ?? ""
+    }
 }
