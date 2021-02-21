01c75408c3918f5ffc09c515dc16c2de2fbf95a5
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-02-21 12:50:25 +0300

    добавил аутлеты и код для отображения во viewDidLoad

--- HomeWork2.8/ViewControllers/ContactProfileViewController.swift
+++ HomeWork2.8/ViewControllers/ContactProfileViewController.swift
@@ -9,18 +9,18 @@ import UIKit
 
 class ContactProfileViewController: UIViewController {
     
-//    Аутлеты для лейблов для отображения информации:
-//    email
-//    phoneNumber
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     
     var person: Person!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = person?.fullName
-//        emailLabel.text = "Email: \(person.email)"
-//        phoneNumberLabel.text = "Phone: \(person.phoneNumber)"
+        fullNameLabel.text = "\(person.fullName)"
+        phoneNumberLabel.text = "\(person.email)"
+        emailLabel.text = "\(person.email)"
     }
     
     
