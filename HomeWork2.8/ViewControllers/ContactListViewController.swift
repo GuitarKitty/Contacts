be6c3a4b42caea1478cada612464305ef76eb904
alex <mirlex1986@gmail.com>
2021-02-22 19:39:45 +0300

    delete garbage

--- HomeWork2.8/ViewControllers/ContactListViewController.swift
+++ HomeWork2.8/ViewControllers/ContactListViewController.swift
@@ -10,28 +10,19 @@ import UIKit
 class ContactListViewController: UITableViewController {
     
     var persons: [Person] = []
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         persons = Person.getContactList()
         tableView.reloadData()
     }
-
+    
     // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         persons.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
@@ -40,66 +31,20 @@ class ContactListViewController: UITableViewController {
         var content = cell.defaultContentConfiguration()
         content.text = person.fullName
         cell.contentConfiguration = content
-
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCell.EditingStyle.delete {
-                persons.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+            persons.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
             DataManager.shared.names.remove(at: indexPath.row)
             DataManager.shared.surnames.remove(at: indexPath.row)
             DataManager.shared.phones.remove(at: indexPath.row)
             DataManager.shared.emails.remove(at: indexPath.row)
-            }
-    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
-    // MARK: - Navigation
-
-// override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//     let profileVC = segue.destination as! ContactProfileViewController
-//     guard  let indexPath = tableView.indexPathForSelectedRow else { return }
-//     let person = persons[indexPath.row]
-//    // profileVC.person = person
-// }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = tableView.indexPathForSelectedRow {
@@ -108,5 +53,5 @@ class ContactListViewController: UITableViewController {
             detailVC.dataManagerID = indexPath.row
         }
     }
-
+    
 }
